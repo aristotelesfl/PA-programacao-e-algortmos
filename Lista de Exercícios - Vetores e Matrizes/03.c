bbb39cb3d415cb894f790b13f3046ffef470b945
Aristóteles Feitosa <125211543+aristotelesfl@users.noreply.github.com>
2023-09-25 10:18:00 -0300

    Create 03.c

---
+++ Lista de Exercícios - Vetores e Matrizes/03.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    float filiais[20], sum=0, media;
+    int index_lucros[20], qtd=0;
+    for (int i; i<5; i++){
+        scanf("%f", &filiais[i]);
+        if(filiais[i]>=0){
+            index_lucros[qtd] = i;
+            sum+=filiais[i];
+            qtd++;
+        }
+    }
+    
+    media = sum/qtd;
+    
+    printf("(a) ");
+    for (int i; i<qtd; i++){
+        printf("%d ", index_lucros[i]);
+    }
+    
+    printf("(b) %.2f", media);
+    return 0;
+}
