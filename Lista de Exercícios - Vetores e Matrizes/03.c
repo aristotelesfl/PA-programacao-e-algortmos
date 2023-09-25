7bf48fe74b79b925232e3c5228a73078da8dd9ed
Aristóteles Feitosa <125211543+aristotelesfl@users.noreply.github.com>
2023-09-25 11:36:30 -0300

    Update 03.c

--- Lista de Exercícios - Vetores e Matrizes/03.c
+++ Lista de Exercícios - Vetores e Matrizes/03.c
@@ -4,7 +4,7 @@ int main()
 {
     float filiais[20], sum=0, media;
     int index_lucros[20], qtd=0;
-    for (int i; i<5; i++){
+    for (int i=0; i<20; i++){
         scanf("%f", &filiais[i]);
         if(filiais[i]>=0){
             index_lucros[qtd] = i;
