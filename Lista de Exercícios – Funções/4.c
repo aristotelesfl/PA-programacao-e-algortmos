d40467fcfd8d2d1a5cdf0a921efd04236483868f
Aristóteles Feitosa <125211543+aristotelesfl@users.noreply.github.com>
2023-12-20 13:37:04 -0300

    Update 4.c

--- Lista de Exercícios – Funções/4.c
+++ Lista de Exercícios – Funções/4.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 
 void chk_primo(int num){
-    int divisores=0;
-    for (int i=1; i<=num; i++){
+    int divisores=0, i=1;
+    while(divisores<=2 && i<=num){
         if(num%i==0){
             divisores++;
         }
+        i++;
     }
-    if(divisores==2 && num!=1) printf("%d - Sim\n", num);
+    if(divisores==2) printf("%d - Sim\n", num);
     else printf("%d - Não\n", num);
 }
 
 int main(void){
-    for (int i=1; i<=1000; i++){
+    for (int i=1; i<=100; i++){
         chk_primo(i);
     }
     return 0;
